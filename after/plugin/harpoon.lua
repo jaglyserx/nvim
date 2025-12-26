b0c881809730f8855d8df5281cdd32292427c688
Jaglyser <joelwblomster@gmail.com>
2025-12-26 09:29:20 +0100

    added tombi lsp for toml foratting etc, also set keybinding for harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -7,11 +7,15 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-8>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-9>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-0>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-+>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
+vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
+
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<C-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-N>", function() harpoon:list():next() end)
